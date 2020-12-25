ae675690a78f260a6d7f02d966032281524701a5
Vicenteguallpa <vincentguallpa@gmail.com>
2020-12-25 15:14:13 -0500

    Added unused libraries

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
