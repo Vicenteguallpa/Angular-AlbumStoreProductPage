a8f9ca786bbbf75d05644903d9b113c166dd26e9
Vicenteguallpa <vincentguallpa@gmail.com>
2020-12-26 12:17:43 -0500

    Displaying a List of Products Complete

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,15 +3,21 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 import { Album } from './album';
+import { Product } from './product'
 
 @Injectable()
 export class ProductService {
   private _albumUrl: string = '../assets/album.json';
+  private _productsUrl: string = '../assets/products.json';
 
   constructor(private _http: HttpClient) { }
 
   getAlbum(id: number) : Observable<Album> {
     return this._http.get<Album>(this._albumUrl);
+  }
+
+  getProducts() : Observable<Product[]>{
+    return this._http.get<Product[]>(this._productsUrl);
   }
 
 }
